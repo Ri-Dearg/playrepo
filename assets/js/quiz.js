66c37fa5ed3155dd4c2a2f34cc4d44b93775144c
Rory Patrick Sheridan <44118951+Ri-Dearg@users.noreply.github.com>
2025-12-18 16:53:58 +0000

    Add basic question loop to quiz

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -3,37 +3,37 @@ const questions = [
     {
         question: 'This is question 1?',
         answers: [
-            'Answer from array',
-            'Answer from array',
-            'Answer from array',
-            'Answer from array',
+            { answer: 'Answer from array', correct: false },
+            { answer: 'Answer from array t', correct: true },
+            { answer: 'Answer from array', correct: false },
+            { answer: 'Answer from array', correct: false },
         ],
     },
     {
         question: 'This is question 2?',
         answers: [
-            'Answer from array',
-            'Answer from array',
-            'Answer from array',
-            'Answer from array',
+            { answer: 'Answer from array', correct: false },
+            { answer: 'Answer from array t', correct: true },
+            { answer: 'Answer from array', correct: false },
+            { answer: 'Answer from array', correct: false },
         ],
     },
     {
         question: 'This is question 3?',
         answers: [
-            'Answer from array',
-            'Answer from array',
-            'Answer from array',
-            'Answer from array',
+            { answer: 'Answer from array', correct: false },
+            { answer: 'Answer from array t', correct: true },
+            { answer: 'Answer from array', correct: false },
+            { answer: 'Answer from array', correct: false },
         ],
     },
     {
         question: 'This is question 4?',
         answers: [
-            'Answer from array',
-            'Answer from array',
-            'Answer from array',
-            'Answer from array',
+            { answer: 'Answer from array', correct: false },
+            { answer: 'Answer from array t', correct: true },
+            { answer: 'Answer from array', correct: false },
+            { answer: 'Answer from array', correct: false },
         ],
     },
 ];
@@ -44,21 +44,63 @@ const instructions = document.getElementById('instructionSection');
 const questionsBox = document.getElementById('questionsBox');
 const answerButtons = document.getElementsByClassName('answerBtn');
 const question = document.getElementById('question');
+const nextButton = document.getElementById('nextQuestion');
+let questionIndex = 0;
+let score = 0;
+
+function updateButton(button) {
+    // Loop through questions array, using the index to access the questions
+    button.innerText = `${questions[questionIndex].answers[i].answer} ${i + 1}`;
+    button.disabled = false;
+}
+
+function nextButtonEvent() {
+    nextButton.classList.remove('d-none');
+    nextButton.addEventListener('click', () => {
+        // Order matters, first make the changes to hidden elements on the page
+        question.innerText = questions[questionIndex].question;
+        for (i = 0; i < answerButtons.length; i++) {
+            updateButton(answerButtons[i]);
+        }
+    });
+}
+
+function disableButtons() {
+    for (let button of answerButtons) {
+        button.disabled = true;
+    }
+    nextButtonEvent();
+}
+
+function isCorrect(i) {
+    console.log(i);
+    console.log(questions[questionIndex].answers);
+}
+
+function answerClick(button, index) {
+    button.addEventListener('click', function (e) {
+        e.target.classList.add('secondary-color');
+        disableButtons();
+        if (questions[questionIndex].answers[index].correct) {
+            score++;
+        }
+        console.log(score);
+        questionIndex++;
+    });
+}
 
 function showAnswers() {
     for (i = 0; i < answerButtons.length; i++) {
         // Loop through questions array, using the index to access the questions
-        answerButtons[i].innerText = questions[0].answers[i];
+        updateButton(answerButtons[i]);
         // Access the event parameter to target the element
-        answerButtons[i].addEventListener('click', function (e) {
-            e.target.classList.add('secondary-color');
-        });
+        answerClick(answerButtons[i], i);
     }
 }
 
 startButton.addEventListener('click', () => {
     // Order matters, first make the changes to hidden elements on the page
-    question.innerText = questions[0].question;
+    question.innerText = questions[questionIndex].question;
     showAnswers();
 
     // Then change display of instructions
